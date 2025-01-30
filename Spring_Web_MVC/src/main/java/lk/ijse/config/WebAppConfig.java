d37292ddee433d484194164cacd97954733404f4
Tashini4 <tashiniwihethunaga@gmail.com>
2025-01-30 20:03:35 +0530

    spring web mvc

--- Spring_Web_MVC/src/main/java/lk/ijse/config/WebAppConfig.java
+++ Spring_Web_MVC/src/main/java/lk/ijse/config/WebAppConfig.java
@@ -9,4 +9,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "lk.ijse.bean" )
 @ComponentScan(basePackages = "lk.ijse.Controller")
 public class WebAppConfig {
+    
 }
