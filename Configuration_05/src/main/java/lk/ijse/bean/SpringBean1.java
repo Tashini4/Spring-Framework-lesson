65bb0b86c1b67846e635ac220a140946b087f101
Tashini4 <tashiniwihethunaga@gmail.com>
2025-01-30 10:36:46 +0530

    spring configuration

---
+++ Configuration_05/src/main/java/lk/ijse/bean/SpringBean1.java
@@ -0,0 +1,7 @@
+package lk.ijse.bean;
+
+public class SpringBean1 {
+    public SpringBean1(){
+        System.out.println("SpringBean1 Constructor");
+    }
+}
---
+++ Configuration_05/src/main/java/lk/ijse/config/AppConfig.java
@@ -0,0 +1,17 @@
+package lk.ijse.config;
+
+import lk.ijse.bean.SpringBean;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@Import({AppConfig1.class})
+/*root
+@ImportResource("classpath:abc.xml")*/
+//if not
+/*@ImportResource("file:absolute-path-of-abc.xml")*/
+public class AppConfig {
+    @Bean
+    public SpringBean springBean(){
+        return new SpringBean();
+    }
+}
