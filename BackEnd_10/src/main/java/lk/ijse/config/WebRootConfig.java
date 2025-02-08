31aac39231e49814ab6f4e9689499794039ef57d
Tashini4 <tashiniwihethunaga@gmail.com>
2025-02-08 21:17:52 +0530

    backend & frontend

---
+++ BackEnd_10/src/main/java/lk/ijse/config/WebRootConfig.java
@@ -0,0 +1,8 @@
+package lk.ijse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+public class WebRootConfig {
+}
